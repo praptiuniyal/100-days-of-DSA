23026cc8c52f53888c770e7fdab1ea21a91f2925
praptiuniyal <uniyalprapti8@gmail.com>
2026-02-03 23:56:15 +0530

    Add files via upload

---
+++ dsa_3.c
@@ -0,0 +1,42 @@
+
+
+//linear search
+
+include <stdio.h>
+
+int main() 
+{
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    int comp = 0;
+    int index= -1;
+
+    for(int i = 0; i < n; i++)
+     {
+        comp++;
+        if(arr[i] == k) {
+            index= i;
+            break;
+        }
+    }
+
+    if(index != -1) 
+    {
+        printf("Found at index %d\n",index);
+    } else {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d", comp);
+
+    return 0;
+}
