f170c7e9b7c2691ec4c6c8eb357f45b4cacf4545
praptiuniyal <uniyalprapti8@gmail.com>
2026-02-08 16:21:17 +0530

    Add files via upload

---
+++ dsa_7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fibo(int n)
+ {
+    if(n == 0)
+     return 0;
+    if(n == 1)
+     return 1;
+    return fibp(n-1) + fibo(n-2);
+}
+
+int main()
+ {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", fibo(n));
+    return 0;
+}
