aacfdbe260c9892066d53091a89ea4d6c23c95c0
praptiuniyal <uniyalprapti8@gmail.com>
2026-02-08 16:07:51 +0530

    Add files via upload

---
+++ dsa_6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    int j = 0;
+    for(int i = 0; i < n; i++)
+     {
+        if(i == 0 || arr[i] != arr[i-1]) 
+        {
+            arr[j++] = arr[i];
+        }
+    }
+
+    for(int i = 0; i < j; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
